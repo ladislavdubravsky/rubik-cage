906676e9d05c6da9969e4f54690b10e72ccd4492
Ladislav <dubravsky90@gmail.com>
2025-08-12 23:25:59 +0200

    rotations

--- src/app/cage.rs
+++ src/app/cage.rs
@@ -1,4 +1,7 @@
-use crate::core::{game::GameState, r#move::Move};
+use crate::core::{
+    game::GameState,
+    r#move::{Layer, Move, Rotation},
+};
 use yew::prelude::*;
 
 #[derive(Properties, PartialEq)]
@@ -11,57 +14,69 @@ pub fn cage(props: &CageProps) -> Html {
     let player_to_move_color = props.game_state.player_to_move.color;
 
     let game_state_handle = props.game_state.clone();
-    let on_flip = Callback::from(move |_| {
+    let apply_move = Callback::from(move |m: Move| {
         let mut new_state = (*game_state_handle).clone();
-        if new_state.apply_move(Move::Flip).is_ok() {
+        if new_state.apply_move(m).is_ok() {
             game_state_handle.set(new_state);
         }
     });
 
     html! {
         <div class="cage">
-            { for (0..3).rev().map(|z| html! {
-                <div class="layer">
-                    <div class="grid">
-                        { for (0..9).map(|i| {
-                            let cubie = props.game_state.cage.grid[i / 3][i % 3][z];
-                            let color = cubie.as_ref().map(|c| c.to_string()).unwrap_or("#444".into());
+            { for [Layer::Up, Layer::Equator, Layer::Down].iter().enumerate().map(|(z, layer)| {
+                html! {
+                    <div class="layer">
+                        <button class="rotate-button" onclick={apply_move.reform(move |_| Move::RotateLayer {
+                            layer: *layer,
+                            rotation: Rotation::CounterClockwise,
+                        })}>{ "↻" }</button>
 
-                            // Cubie drop moves are implemented by clicking on top layer slots.
-                            let onclick = if z == 2 && i != 4 && cubie.is_none() {
-                                let game_state_handle = props.game_state.clone();
-                                let color = player_to_move_color.clone();
+                        <div class="grid">
+                            { for (0..9).map(|i| {
+                                let cubie = props.game_state.cage.grid[i / 3][i % 3][2 - z];
+                                let color = cubie.as_ref().map(|c| c.to_string()).unwrap_or("#444".into());
 
-                                Some(Callback::from(move |_| {
-                                    let mut new_state = (*game_state_handle).clone();
-                                    let res = new_state.apply_move(Move::Drop {
-                                        color,
-                                        column: (i / 3, i % 3),
-                                    });
-                                    if res.is_ok() {
-                                        game_state_handle.set(new_state);
-                                    }
-                                }))
-                            } else {
-                                None
-                            };
+                                // Cubie drops are implemented by clicking on top layer slots.
+                                let onclick = if z == 0 && i != 4 && cubie.is_none() {
+                                    let game_state_handle = props.game_state.clone();
+                                    let color = player_to_move_color.clone();
 
-                            html! {
-                                <div
-                                    class={classes!(
-                                        "slot",
-                                        if z == 2 && i != 4 && cubie.is_none() { "top-slot" } else { "" },
-                                        if i == 4 { "center-slot" } else { "" }
-                                    )}
-                                    style={format!("--slot-color: {color}; --slot-hover-color: {player_to_move_color};")}
-                                    onclick={onclick}
-                                />
-                            }
-                        }) }
+                                    Some(Callback::from(move |_| {
+                                        let mut new_state = (*game_state_handle).clone();
+                                        let res = new_state.apply_move(Move::Drop {
+                                            color,
+                                            column: (i / 3, i % 3),
+                                        });
+                                        if res.is_ok() {
+                                            game_state_handle.set(new_state);
+                                        }
+                                    }))
+                                } else {
+                                    None
+                                };
+
+                                html! {
+                                    <div
+                                        class={classes!(
+                                            "slot",
+                                            if z == 0 && i != 4 && cubie.is_none() { "top-slot" } else { "" },
+                                            if i == 4 { "center-slot" } else { "" }
+                                        )}
+                                        style={format!("--slot-color: {color}; --slot-hover-color: {player_to_move_color};")}
+                                        onclick={onclick}
+                                    />
+                                }
+                            }) }
+                        </div>
+
+                        <button class="rotate-button" onclick={apply_move.reform(move |_| Move::RotateLayer {
+                            layer: *layer,
+                            rotation: Rotation::Clockwise,
+                        })}>{ "↺" }</button>
                     </div>
-                </div>
+                }
             }) }
-            <button onclick={on_flip}>{ "Flip" }</button>
+            <button onclick={apply_move.reform(|_| Move::Flip)}>{ "Flip" }</button>
         </div>
     }
 }
